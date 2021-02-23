fa02c82982ddcd9ca85e2859ef2fd0b7dbf96537
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-02-23 11:45:04 +0900

    DOM 이용 html객체 접근

--- login-app/src/public/js/home/login.js
+++ login-app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
 
 console.log("log in success");
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.nodeValue,
+    password: password.nodeValue,
+  };
+}
