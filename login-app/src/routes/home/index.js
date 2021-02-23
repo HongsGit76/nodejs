59cffe886e203c93775b4336f90024a9ac812fd7
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-02-23 20:23:25 +0900

    Update index.js

--- login-app/src/routes/home/index.js
+++ login-app/src/routes/home/index.js
@@ -10,7 +10,6 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.hello);
-
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.process.login);
 
